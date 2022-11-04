9b9187d6fd180267b2d9a9c22dc9722d67cc3943
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-04 10:36:08 +0100

    Debug log

--- CreateRspecIssue/index.ts
+++ CreateRspecIssue/index.ts
@@ -3,7 +3,8 @@ import { OctokitAction } from "../lib/OctokitAction";
 class CreateRspecIssue extends OctokitAction {
     protected async execute() {
         this.log("Creating issue");
-        await this.octokit.rest.issues.create(this.addRepo({ title: `Update RSPEC before ${this.payload.milestone.title} release`, milestone: this.payload.milestone.number, labels: ["Type: Tooling"] }));
+        const issue = await this.octokit.rest.issues.create(this.addRepo({ title: `Update RSPEC before ${this.payload.milestone.title} release`, milestone: this.payload.milestone.number, labels: ["Type: Tooling"] }));
+        this.log(this.serializeToString(issue.data));
     }
 }
 
