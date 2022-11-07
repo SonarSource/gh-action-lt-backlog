38137347ae7abb58fafcce2a16e823c2b9ceddc7
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-07 12:29:45 +0100

    Improve "any" to "as"

--- CreateCardForStandalonePR/CreateCardForStandalonePR.ts
+++ CreateCardForStandalonePR/CreateCardForStandalonePR.ts
@@ -1,8 +1,9 @@
+import { WebhookPayload } from "@actions/github/lib/interfaces";
 import { OctokitAction } from "../lib/OctokitAction";
 
 class CreateCardForStandalonePR extends OctokitAction {
     protected async execute(): Promise<void> {
-        const pr:any = this.payload.pull_request;
+        const pr = this.payload.pull_request as WebhookPayload["pull_request"] & { id: number };
         const matches = pr.body == null ? null : pr.body.match(/(close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)\s*#\d+/gi);
         if (matches) {
             this.log("Skip, contains '" + matches[0] + "'");
