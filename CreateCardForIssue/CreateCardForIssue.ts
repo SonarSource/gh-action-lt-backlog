6b8a1531d2fb1001e6230819044c98f5647c30bf
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-15 14:31:43 +0100

    Log skip

--- CreateCardForIssue/CreateCardForIssue.ts
+++ CreateCardForIssue/CreateCardForIssue.ts
@@ -8,7 +8,9 @@ class CreateCardForIssue extends OctokitAction {
         const column_id = this.getInputNumber("column-id");
         const project = await ProjectContent.FromColumn(this, column_id);
         const issue = this.payload.issue as IssueOrPR;
-        if (!(await project.findCard(issue))) {
+        if (await project.findCard(issue)) {
+            this.log("Skip, card already exists");
+        } else {
             await this.createCard(issue, column_id);
         }
     }
