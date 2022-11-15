664e68dce188e8b716a19dfeab188aba6ca54237
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-15 11:57:45 +0100

    Prototype

--- CreateCardForIssue/CreateCardForIssue.ts
+++ CreateCardForIssue/CreateCardForIssue.ts
@@ -3,8 +3,9 @@ import { OctokitAction } from "../lib/OctokitAction";
 
 class CreateCardForIssue extends OctokitAction {
 
-    protected execute(): Promise<void> {
-        throw new Error("Method not implemented.");
+    protected async execute(): Promise<void> {
+        const column_id = this.getInputNumber("column-id");
+        await this.createCard(this.payload.issue as IssueOrPR, column_id);
     }
 }
 
