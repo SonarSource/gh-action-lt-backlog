020b4078f710ad009d7183dba384b9bf4944e786
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-11-28 13:26:03 +0100

    Debug log

--- LockBranch/LockBranch.js
+++ LockBranch/LockBranch.js
@@ -12,10 +12,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const OctokitAction_1 = require("../lib/OctokitAction");
 class LockBranch extends OctokitAction_1.OctokitAction {
     execute() {
+        var _a, _b, _c, _d, _e;
         return __awaiter(this, void 0, void 0, function* () {
             const lock_branch = this.getInputBoolean("lock");
             const branch = this.getInput("branch-name");
-            yield this.rest.repos.updateBranchProtection(this.addRepo({ branch, lock_branch }));
+            //const { data: branchProtection } = await this.rest.repos.getBranchProtection(this.addRepo({ branch }));
+            var arg = this.addRepo({ branch, lock_branch });
+            this.log("--- getAdminBranchProtection ---");
+            this.logSerialized((_a = (yield this.rest.repos.getAdminBranchProtection(arg))) === null || _a === void 0 ? void 0 : _a.data);
+            this.log("--- getBranchProtection ---");
+            this.logSerialized((_b = (yield this.rest.repos.getBranchProtection(arg))) === null || _b === void 0 ? void 0 : _b.data);
+            this.log("--- getCommitSignatureProtection ---");
+            this.logSerialized((_c = (yield this.rest.repos.getCommitSignatureProtection(arg))) === null || _c === void 0 ? void 0 : _c.data);
+            this.log("--- getPullRequestReviewProtection ---");
+            this.logSerialized((_d = (yield this.rest.repos.getPullRequestReviewProtection(arg))) === null || _d === void 0 ? void 0 : _d.data);
+            this.log("--- getStatusChecksProtection ---");
+            this.logSerialized((_e = (yield this.rest.repos.getStatusChecksProtection(arg))) === null || _e === void 0 ? void 0 : _e.data);
         });
     }
 }
--- LockBranch/LockBranch.ts
+++ LockBranch/LockBranch.ts
@@ -5,7 +5,22 @@ class LockBranch extends OctokitAction {
     protected async execute(): Promise<void> {
         const lock_branch = this.getInputBoolean("lock");
         const branch = this.getInput("branch-name");
-        await this.rest.repos.updateBranchProtection(this.addRepo({ branch, lock_branch }));
+        //const { data: branchProtection } = await this.rest.repos.getBranchProtection(this.addRepo({ branch }));
+        var arg = this.addRepo({ branch, lock_branch });
+        this.log("--- getAdminBranchProtection ---");
+        this.logSerialized((await this.rest.repos.getAdminBranchProtection(arg))?.data);
+        this.log("--- getBranchProtection ---");
+        this.logSerialized((await this.rest.repos.getBranchProtection(arg))?.data);
+        this.log("--- getCommitSignatureProtection ---");
+        this.logSerialized((await this.rest.repos.getCommitSignatureProtection(arg))?.data);
+        this.log("--- getPullRequestReviewProtection ---");
+        this.logSerialized((await this.rest.repos.getPullRequestReviewProtection(arg))?.data);
+        this.log("--- getStatusChecksProtection ---");
+        this.logSerialized((await this.rest.repos.getStatusChecksProtection(arg))?.data);
+
+
+        //await this.rest.repos.updateBranchProtection(this.addRepo({ branch, lock_branch }));
+        //this.log(`Branch '${branch}' was ${lock_branch ? "locked" : "unlocked and open for changes"}.`);
     }
 }
 
