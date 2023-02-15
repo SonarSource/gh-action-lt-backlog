8bfafe8137933f931c92f6c3a278b1b59fd677da
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-02-15 08:16:45 +0100

    add prettier

--- src/CreateRspecIssue/CreateRspecIssue.ts
+++ src/CreateRspecIssue/CreateRspecIssue.ts
@@ -1,17 +1,18 @@
-import { OctokitAction } from "../lib/OctokitAction";
+import { OctokitAction } from '../lib/OctokitAction';
 
 class CreateRspecIssue extends OctokitAction {
-
-    protected async execute(): Promise<void> {
-        this.log("Creating issue");
-        const { data: issue } = await this.rest.issues.create(this.addRepo({
-            title: `Update RSPEC before ${this.payload.milestone.title} release`,
-            milestone: this.payload.milestone.number,
-            labels: ["Type: Tooling"]
-        }));
-        this.log(`Created issue #${issue.number}: ${issue.html_url}`);
-        await this.createCard(issue, this.getInputNumber("column-id"));
-    }
+  protected async execute(): Promise<void> {
+    this.log('Creating issue');
+    const { data: issue } = await this.rest.issues.create(
+      this.addRepo({
+        title: `Update RSPEC before ${this.payload.milestone.title} release`,
+        milestone: this.payload.milestone.number,
+        labels: ['Type: Tooling'],
+      }),
+    );
+    this.log(`Created issue #${issue.number}: ${issue.html_url}`);
+    await this.createCard(issue, this.getInputNumber('column-id'));
+  }
 }
 
 const action = new CreateRspecIssue();
--- src/lib/Action.ts
+++ src/lib/Action.ts
@@ -1,50 +1,48 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { Context } from "@actions/github/lib/context";
-import { WebhookPayload } from "@actions/github/lib/interfaces";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Context } from '@actions/github/lib/context';
+import { WebhookPayload } from '@actions/github/lib/interfaces';
 
 export type Repo = {
-    owner: string;
-    repo: string;
-}
+  owner: string;
+  repo: string;
+};
 
 export abstract class Action {
+  protected readonly context: Context;
+  protected readonly repo: Repo;
+  protected readonly payload: WebhookPayload;
 
-    protected readonly context: Context;
-    protected readonly repo: Repo;
-    protected readonly payload: WebhookPayload;
+  protected abstract execute(): Promise<void>;
 
-    protected abstract execute(): Promise<void>;
+  public constructor() {
+    this.context = github.context;
+    this.repo = github.context.repo;
+    this.payload = github.context.payload;
+  }
 
-    public constructor() {
-        this.context = github.context;
-        this.repo = github.context.repo;
-        this.payload = github.context.payload
+  public async run(): Promise<void> {
+    try {
+      await this.execute();
+      this.log('Done');
+    } catch (ex) {
+      core.setFailed(ex.message);
     }
+  }
 
-    public async run(): Promise<void> {
-        try {
-            await this.execute();
-            this.log("Done");
-        }
-        catch (ex) {
-            core.setFailed(ex.message);
-        }
-    }
+  public log(line: string) {
+    console.log(line);
+  }
 
-    public log(line: string) {
-        console.log(line);
-    }
+  public logSerialized(value: any) {
+    console.log(this.serializeToString(value));
+  }
 
-    public logSerialized(value: any) {
-        console.log(this.serializeToString(value));
-    }
+  public addRepo(other: any): any {
+    return { ...this.repo, ...other };
+  }
 
-    public addRepo(other: any): any {
-        return { ...this.repo, ...other };
-    }
-
-    private serializeToString(value: any): string {
-        return JSON.stringify(value, undefined, 2);
-    }
+  private serializeToString(value: any): string {
+    return JSON.stringify(value, undefined, 2);
+  }
 }
