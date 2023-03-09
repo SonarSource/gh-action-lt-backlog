9d69223ac44abe928bfbba8f5b7037ccec70b58a
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-03-09 07:09:32 +0100

    write check - first attempt

---
+++ scripts/check_compiled.js
@@ -0,0 +1,20 @@
+import { execSync } from 'child_process';
+
+execSync('npm run build');
+
+(async () => {
+  const status = await simpleGit().status();
+  if (!isEmpty(status)) {
+    throw new Error(`There are uncommited compiled files: ${JSON.stringify(status, null, 2)}`);
+  }
+})();
+
+
+function isEmpty(gitStatus) {
+  return (
+    gitStatus.not_added.length === 0 &&
+    gitStatus.conflicted.length === 0 &&
+    gitStatus.created.length === 0 &&
+    gitStatus.deleted.length === 0
+  );
+}
