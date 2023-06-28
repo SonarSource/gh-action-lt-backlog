7cba3751f42e621e76e9a2d2aaf684c013410548
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2023-06-28 14:16:53 +0200

    Don't assign to a group (#49)

--- MoveCardToReview/MoveCardToReview.ts
+++ MoveCardToReview/MoveCardToReview.ts
@@ -4,7 +4,12 @@ import { IssueOrPR } from '../lib/IssueOrPR';
 class MoveCardToReview extends PullRequestAction {
   protected async processReassignment(issueOrPR: IssueOrPR): Promise<void> {
     if (issueOrPR.state === 'open') {
-      await this.reassignIssue(issueOrPR, this.payload.requested_reviewer.login);
+      const login = this.payload.requested_reviewer.login;
+      if (login) {
+        await this.reassignIssue(issueOrPR, login);
+      } else {  // Review requested from a group - keep it unassigned to raise a suspicion about the card
+        await this.removeAssignees(issueOrPR);
+      }
     }
   }
 }
