4a7a0d3b53d1b999b787edcaa9a052ee29cdd913
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2023-06-29 10:14:47 +0200

    Add CopyLabelToPR (#56)

--- lib/OctokitAction.ts
+++ lib/OctokitAction.ts
@@ -93,24 +93,27 @@ export abstract class OctokitAction extends Action {
     }
   }
 
-  protected fixedIssues(pr: { body?: string }): number[] {
-    const fixes = "(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)";
-    const url = this.escapeRegex(`https://github.com/${this.repo.owner}/${this.repo.repo}/issues/`);
-    const capturingId = "(\\d+)";
-    const issueId = "#" + capturingId;
-    const issueUrl = url + capturingId;
-    const issueLink = `\\[[^\\]]+\\]\\(${url}${capturingId}\\)`;
-    const pattern = `${fixes}\\s*(?:${issueId}|${issueUrl}|${issueLink})`;
-    const regex = new RegExp(pattern, "gi");
-    let result = [];
-    let match;
-    while (match = regex.exec(pr.body)) {
-      result = result.concat(match.map(x => parseInt(x)).filter(x => !isNaN(x)));
+  protected async addLabels(issue: { number: number } , labels: string[]): Promise<void> {
+    if (labels.length === 0) {
+      console.log(`Skipping adding labels to #${issue.number}`)
+    } else {
+      console.log(`Adding ${labels.length} label(s) to #${issue.number}`)
+      await this.rest.issues.addLabels(this.addRepo({
+        issue_number: issue.number,
+        labels
+      }));
     }
-    return result;
   }
 
-  public async createCard(issueOrPR: IssueOrPR, column_id: number): Promise<void> {
+  protected fixedIssues(pr: { body?: string }): number[] {
+    return this.mentionedIssuesCore(pr, "(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)");
+  }
+
+  protected mentionedIssues(pr: { body?: string }): number[] {
+    return this.mentionedIssuesCore(pr, "");
+  }
+
+  protected async createCard(issueOrPR: IssueOrPR, column_id: number): Promise<void> {
     if (column_id === 0) {
       this.log(`Skip creating card for #${issueOrPR.number}. column_id was not set.`);
     } else {
@@ -119,13 +122,44 @@ export abstract class OctokitAction extends Action {
     }
   }
 
-  public async createNote(note: string, column_id: number): Promise<void> {
+  protected async createNote(note: string, column_id: number): Promise<void> {
     if (column_id === 0) {
       this.log("Skip creating card for note. column_id was not set.");
     } else {
       this.log(`Creating note in column ${column_id}`);
       await this.rest.projects.createCard({ column_id, note });
     }
+  }
+
+  /**
+   * This method will extract mentioned issue IDs (that are automatically translated into issue links in GitHub UI) from a PR body. The text like
+   * ```
+   * Fixes #42 because https://github.com/SonarSource/this-repo/issues/41 broke the logic that [link](https://github.com/SonarSource/this-repo/issues/40) added.
+   * ```
+   * Supported formats:
+   * regexPrefix #42
+   * regexPrefix https://github.com/SonarSource/this-repo/issues/42
+   * regexPrefix [text](https://github.com/SonarSource/this-repo/issues/42)
+   * 
+   * Full link URLs must point to the current repo. GitHub issues from other repositories will not be returned.
+   * 
+   * @param regexPrefix Regular expression that should be prefixed before the actual issue mention.
+   * @returns Array of mentioned issue numbers. The example above would return 42, 41, 40.
+   */
+  protected mentionedIssuesCore(pr: { body?: string }, regexPrefix: string): number[] {
+    const url = this.escapeRegex(`https://github.com/${this.repo.owner}/${this.repo.repo}/issues/`);
+    const capturingId = "(\\d+)";
+    const issueId = "#" + capturingId;
+    const issueUrl = url + capturingId;
+    const issueLink = `\\[[^\\]]+\\]\\(${url}${capturingId}\\)`;
+    const pattern = `${regexPrefix}\\s*(?:${issueId}|${issueUrl}|${issueLink})`
+    const regex = new RegExp(pattern, "gi");
+    let result = [];
+    let match;
+    while (match = regex.exec(pr.body)) {
+      result = result.concat(match.map(x => parseInt(x)).filter(x => !isNaN(x)));
+    }
+    return result;
   }
 
   private escapeRegex(string): string {
