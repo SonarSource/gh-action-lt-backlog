6acd5e758f6a57eebd478cc8459ca4995f9d4015
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-06-30 13:59:49 +0200

    update dist

---
+++ dist/MoveCardToReview-v2/MoveCardToReviewV2.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const PullRequestActionV2_1 = require("../lib/PullRequestActionV2");
+/**
+ * 1. move card
+ *  1. column_id is known
+ *  2. write mutation call
+ *    params: issueId, user_id to remove, user_id to add
+ * 2. change assignees
+ *   1. remove assignee
+ *   2. set assignee
+ */
+class MoveCardToReviewV2 extends PullRequestActionV2_1.PullRequestActionV2 {
+    async processReassignment(issueOrPR) {
+        if (issueOrPR.state === 'open') {
+            const login = this.payload.requested_reviewer.login;
+            if (login) {
+                await this.reassignIssueV2(issueOrPR, login, issueOrPR.assignees.edges[0].node.id);
+            }
+            else { // Review requested from a group - keep it unassigned to raise a suspicion about the card
+                await this.removeAssignees(issueOrPR);
+            }
+        }
+    }
+    async reassignIssueV2(issueOrPr, loginToAdd, loginToRemove) {
+        const query = {
+            query: `
+      mutation($newUserId: ID! $oldUserId: ID! $issueId: ID!) {
+        removeAssigneesFromAssignable(input: {
+          assignableId: $issueId
+          assigneeIds: [$oldUserId]
+        }) {
+          assignable {
+            assignees(last: 1) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+        addAssigneesToAssignable(input: {
+          assignableId: $issueId
+          assigneeIds: [$newUserId]
+          clientMutationId: "gh action"
+        }) {
+          assignable {
+            assignees(last: 10) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+      `,
+            newUserId: loginToAdd,
+            oldUserId: loginToRemove,
+            issueId: issueOrPr.id,
+        };
+        const response = await this.sendGraphQL(query);
+        this.log(`reassigned assignees: ` + {
+            newUserId: loginToAdd,
+            oldUserId: loginToRemove,
+            issueId: issueOrPr.id,
+        });
+        this.logSerialized(response);
+    }
+    async removeAssignees(issue) {
+    }
+}
+const action = new MoveCardToReviewV2();
+action.run();
+//# sourceMappingURL=MoveCardToReviewV2.js.map
---
+++ dist/lib/PullRequestActionV2.js
@@ -0,0 +1,85 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PullRequestActionV2 = void 0;
+const GraphQLAction_1 = require("./GraphQLAction");
+/**
+ * 1. move card
+ *  1.1. column_id is known
+ *  1.2. write mutation call
+ *    params: issueId, user_id to remove, user_id to add
+ *    1.2.1: get issueId
+ *      retrieve issueId from:
+ *        - payload.repository.name
+ *        - payload.repository.owner
+ *        - issues(payload.pr.body)
+ *      retrieve project Id from "get column_ids" call
+ *      retrieve all issues for a project using "get issues" call
+ *      having retrieved the issue number, use it to retrieve the issue id
+ *    1.2.2: get old_user_id -
+ *    1.2.3: get new user_id -
+ * 2. change assignees
+ *   1. remove assignee
+ *   2. set assignee
+ */
+class PullRequestActionV2 extends GraphQLAction_1.GraphQLAction {
+    async execute() {
+        const column_id = this.getInputNumber('column-id');
+        //const project = ProjectContent.fromColumn(this, column_id);
+        let isProcessPR = true;
+        const pr = this.payload.pull_request;
+        const repo = this.payload.repository;
+        const fixedIssues = this.fixedIssues(pr);
+        for (const fixedIssue of fixedIssues) {
+            let linkedIssue = await this.getIssueV2(repo.name, repo.owner.login, fixedIssue);
+            if (linkedIssue) {
+                isProcessPR = false;
+                await this.processIssue(column_id, linkedIssue);
+            }
+        }
+        if (isProcessPR) {
+            const fullPR = await this.getPullRequest(pr.number);
+            if (fullPR) {
+                await this.processIssue(column_id, fullPR);
+            }
+        }
+    }
+    async getIssueV2(repositoryName, repositoryOwner, issueNumber) {
+        const query = {
+            query: `
+        query ($repoName: String!, $owner: String!, $issueNumber: Int! ) {
+          repository(name: $repoName, owner: $owner) {
+            issue: issue(number: $issueNumber) {
+              title
+              createdAt
+              id
+              state
+              assignees(first: 10) {
+                edges {
+                  node {
+                    id
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+            repoName: repositoryName,
+            owner: repositoryOwner,
+            issueNumber,
+        };
+        const { data: { repository: issue } } = await this.sendGraphQL(query);
+        this.log(`retrieved issue`);
+        this.logSerialized(issue);
+        return issue;
+    }
+    async processIssue(column_id, issueOrPR) {
+        await this.processReassignment(issueOrPR);
+        if (issueOrPR.state === 'open') {
+            //await project.moveOrCreateCard(issueOrPR, column_id);
+        }
+    }
+}
+exports.PullRequestActionV2 = PullRequestActionV2;
+//# sourceMappingURL=PullRequestActionV2.js.map
