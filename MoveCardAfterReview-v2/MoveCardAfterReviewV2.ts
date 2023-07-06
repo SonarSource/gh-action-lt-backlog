398568e713335674a8862ca5c67349886bd340ac
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-07-06 15:49:17 +0200

    fix fetching id

--- MoveCardAfterReview-v2/MoveCardAfterReviewV2.ts
+++ MoveCardAfterReview-v2/MoveCardAfterReviewV2.ts
@@ -4,8 +4,10 @@ import { PullRequestActionV2 } from '../lib/PullRequestActionV2';
 
 class MoveCardAfterReviewV2 extends PullRequestActionV2 {
   protected async processReassignment(issueOrPR: IssueOrPR): Promise<void> {
+    const login = this.payload.requested_reviewer.login;
+    const newUserId = await this.getUserId(login);
     const oldUserIds = issueOrPR.assignees.map(assignee => assignee.id);
-    await this.reassignIssueV2(issueOrPR, this.payload.pull_request.user.login, oldUserIds); // Also for closed issues
+    await this.reassignIssueV2(issueOrPR, newUserId, oldUserIds); // Also for closed issues
   }
 }
 
--- dist/MoveCardAfterReview-v2/MoveCardAfterReviewV2.js
+++ dist/MoveCardAfterReview-v2/MoveCardAfterReviewV2.js
@@ -3,8 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const PullRequestActionV2_1 = require("../lib/PullRequestActionV2");
 class MoveCardAfterReviewV2 extends PullRequestActionV2_1.PullRequestActionV2 {
     async processReassignment(issueOrPR) {
+        const login = this.payload.requested_reviewer.login;
+        const newUserId = await this.getUserId(login);
         const oldUserIds = issueOrPR.assignees.map(assignee => assignee.id);
-        await this.reassignIssueV2(issueOrPR, this.payload.pull_request.user.login, oldUserIds); // Also for closed issues
+        await this.reassignIssueV2(issueOrPR, newUserId, oldUserIds); // Also for closed issues
     }
 }
 const action = new MoveCardAfterReviewV2();
