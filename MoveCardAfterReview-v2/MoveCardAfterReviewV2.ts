265a963118b27c86e00fd5d0ac88d7e3358a7e2f
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-07-06 16:05:35 +0200

    fix user

--- MoveCardAfterReview-v2/MoveCardAfterReviewV2.ts
+++ MoveCardAfterReview-v2/MoveCardAfterReviewV2.ts
@@ -4,7 +4,7 @@ import { PullRequestActionV2 } from '../lib/PullRequestActionV2';
 
 class MoveCardAfterReviewV2 extends PullRequestActionV2 {
   protected async processReassignment(issueOrPR: IssueOrPR): Promise<void> {
-    const login = this.payload.requested_reviewer.login;
+    const login = this.payload.pull_request.user.login;
     const newUserId = await this.getUserId(login);
     const oldUserIds = issueOrPR.assignees.map(assignee => assignee.id);
     await this.reassignIssueV2(issueOrPR, newUserId, oldUserIds); // Also for closed issues
