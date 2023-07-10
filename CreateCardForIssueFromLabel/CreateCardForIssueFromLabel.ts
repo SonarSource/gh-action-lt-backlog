05576e4edea8b6f89d9ac7b70234434e20fe7f2d
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-07-10 16:49:26 +0200

    Write CreateCardForIssueFromLabel for GraphQL

--- CreateCardForIssueFromLabel/CreateCardForIssueFromLabel.ts
+++ CreateCardForIssueFromLabel/CreateCardForIssueFromLabel.ts
@@ -1,13 +1,13 @@
 import { IssueOrPR } from '../lib/IssueOrPR';
 import { OctokitAction } from '../lib/OctokitAction';
-import { ProjectContentV1 } from '../lib/ProjectContent';
+import { ProjectContentV2 } from '../lib/ProjectContent';
 
 class CreateCardForIssueFromLabel extends OctokitAction {
   protected async execute(): Promise<void> {
     const labelPrefix = this.getInput('label-prefix');
     const labelName = this.payload.label.name;
     if (labelName.startsWith(labelPrefix)) {
-      const project = await ProjectContentV1.fromProject(this, this.getInputNumber('project-id'));
+      const project = await ProjectContentV2.fromProject(this, this.getInputNumber('project-number'));
       const columnName = labelName.substring(labelPrefix.length).trim();
       const column = project.columnFromName(columnName);
       if (column) {
