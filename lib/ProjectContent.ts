666b9e72dc669e28fbe30c833e8d12c7569a6fd0
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-07-25 14:19:15 +0200

    Fix draft cards

--- lib/ProjectContent.ts
+++ lib/ProjectContent.ts
@@ -159,7 +159,7 @@ export class ProjectContentV2 extends ProjectContent {
     while (hasNextPage) {
       const items = await this.loadPaginatedCardItems(endCursor);
       for (const node of items.nodes) {
-        if (node.content.number === issueOrPR.number) {
+        if (node.content?.number === issueOrPR.number) {
           return { id: node.id, columnName: node.fieldValueByName?.name };
         }
       }
