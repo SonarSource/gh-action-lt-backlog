f52b9dd451cc9004798ef73d25b09c9eb768c87d
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-07-22 14:02:19 +0200

    Remove v1 GitHub Kanban actions (#89)

--- dist/lib/OctokitAction.js
+++ dist/lib/OctokitAction.js
@@ -4,7 +4,6 @@ exports.OctokitAction = void 0;
 const core = require("@actions/core");
 const github = require("@actions/github");
 const Action_1 = require("./Action");
-const ProjectContent_1 = require("./ProjectContent");
 const node_fetch_1 = require("node-fetch");
 const graphql_1 = require("@octokit/graphql");
 class OctokitAction extends Action_1.Action {
@@ -39,20 +38,6 @@ class OctokitAction extends Action_1.Action {
     getInputBoolean(name) {
         return this.getInput(name).toLowerCase() === 'true';
     }
-    async downloadData(url) {
-        this.log('Downloading ' + url);
-        return (await this.octokit.request(url)).data;
-    }
-    async getIssue(issue_number) {
-        try {
-            this.log(`Getting issue #${issue_number}`);
-            return (await this.rest.issues.get(this.addRepo({ issue_number }))).data;
-        }
-        catch (error) {
-            this.log(`Issue #${issue_number} not found: ${error}`);
-            return null;
-        }
-    }
     async getPullRequest(pull_number) {
         try {
             this.log(`Getting PR #${pull_number}`);
@@ -63,98 +48,6 @@ class OctokitAction extends Action_1.Action {
             return null;
         }
     }
-    async addAssignee(issue, login) {
-        this.log('Assigning to: ' + login);
-        await this.rest.issues.addAssignees(this.addRepo({
-            issue_number: issue.number,
-            assignees: [login],
-        }));
-    }
-    async removeAssignees(issue) {
-        const oldAssignees = issue.assignees.map(x => x.login);
-        if (oldAssignees.length !== 0) {
-            this.log('Removing assignees: ' + oldAssignees.join(', '));
-            await this.rest.issues.removeAssignees(this.addRepo({
-                issue_number: issue.number,
-                assignees: oldAssignees,
-            }));
-        }
-    }
-    async reassignIssue(issue, login) {
-        if (login.includes("[bot]")) { // Avoid "dependabot[bot]"
-            this.log(`Skipping reassignment to: ${login}`);
-        }
-        else {
-            await this.removeAssignees(issue);
-            await this.addAssignee(issue, login);
-        }
-    }
-    async addLabels(issue, labels) {
-        if (labels.length === 0) {
-            this.log(`Skipping adding labels to #${issue.number}`);
-        }
-        else {
-            this.log(`Adding ${labels.length} label(s) to #${issue.number}`);
-            await this.rest.issues.addLabels(this.addRepo({
-                issue_number: issue.number,
-                labels
-            }));
-        }
-    }
-    fixedIssues(pr) {
-        return this.mentionedIssuesCore(pr, "(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)");
-    }
-    mentionedIssues(pr) {
-        return this.mentionedIssuesCore(pr, "");
-    }
-    async createCard(issueOrPR, column_id) {
-        if (column_id === 0) {
-            this.log(`Skip creating card for #${issueOrPR.number}. column_id was not set.`);
-        }
-        else {
-            const project = await ProjectContent_1.ProjectContentV1.fromColumn(this, column_id);
-            project.createCard(issueOrPR, column_id);
-        }
-    }
-    async createNote(note, column_id) {
-        if (column_id === 0) {
-            this.log("Skip creating card for note. column_id was not set.");
-        }
-        else {
-            this.log(`Creating note in column ${column_id}`);
-            await this.rest.projects.createCard({ column_id, note });
-        }
-    }
-    /**
-     * This method will extract mentioned issue IDs (that are automatically translated into issue links in GitHub UI) from a PR body. The text like
-     * ```
-     * Fixes #42 because https://github.com/SonarSource/this-repo/issues/41 broke the logic that [link](https://github.com/SonarSource/this-repo/issues/40) added.
-     * ```
-     * Supported formats:
-     * regexPrefix #42
-     * regexPrefix https://github.com/SonarSource/this-repo/issues/42
-     * regexPrefix [text](https://github.com/SonarSource/this-repo/issues/42)
-     *
-     * Full link URLs must point to the current repo. GitHub issues from other repositories will not be returned.
-     *
-     * @param regexPrefix Regular expression that should be prefixed before the actual issue mention.
-     * @returns Array of mentioned issue numbers. The example above would return 42, 41, 40.
-     */
-    mentionedIssuesCore(pr, regexPrefix) {
-        const url = this.escapeRegex(`https://github.com/${this.repo.owner}/${this.repo.repo}/issues/`);
-        const capturingId = "(\\d+)";
-        const issueId = "#" + capturingId;
-        const issueUrl = url + capturingId;
-        const issueLink = `\\[[^\\]]+\\]\\(${url}${capturingId}\\)`;
-        const pattern = `${regexPrefix}\\s*(?:${issueId}|${issueUrl}|${issueLink})`;
-        const regex = new RegExp(pattern, "gi");
-        let result = [];
-        let match;
-        while (match = regex.exec(pr.body)) {
-            result = result.concat(match.map(x => parseInt(x)).filter(x => !isNaN(x)));
-        }
-        return result;
-    }
     async sendSlackMessage(text) {
         const channel = this.getInput("slack-channel");
         if (channel) {
@@ -164,9 +57,6 @@ class OctokitAction extends Action_1.Action {
         else {
             this.log("Skip sending slack message, channel was not set.");
         }
-    }
-    escapeRegex(string) {
-        return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
     }
     async sendSlackPost(url, jsonRequest) {
         const token = this.getInput("slack-token");
--- lib/OctokitTypes.ts
+++ lib/OctokitTypes.ts
@@ -1,7 +1,3 @@
 import { components } from '@octokit/openapi-types/types.d';
 
-export type Issue = components['schemas']['issue'];
-export type Label = components['schemas']['label'];
-export type ProjectCard = components['schemas']['project-card'];
-export type ProjectColumn = components['schemas']['project-column'];
 export type PullRequest = components['schemas']['pull-request'];
