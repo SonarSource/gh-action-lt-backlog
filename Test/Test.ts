b0158e4cd031d5c7b768f0fdfe2cd62d54dd4019
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-07-30 15:53:27 +0200

    Remove authlog

--- Test/Test.ts
+++ Test/Test.ts
@@ -9,10 +9,10 @@ class Test extends OctokitAction {
     //const update = { title: pr.title + '.' };
     //await this.rest.pulls.update(this.addRepo({ pull_number: 600, ...update }));
 
-    this.log("Requesting getAuthenticated");
-    var me = await this.rest.users.getAuthenticated();
-    this.log("Received getAuthenticated");
-    this.logSerialized(me);
+    //this.log("Requesting getAuthenticated");
+    //var me = await this.rest.users.getAuthenticated();
+    //this.log("Received getAuthenticated");
+    //this.logSerialized(me);
 
 
 
