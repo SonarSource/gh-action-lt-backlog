8a9df8282caee5fed40f3a7ef0b1f706a46e51ab
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-07-31 19:06:24 +0200

    When issue is created, remove duplicate whitespace (#115)

--- PullRequestCreated/PullRequestCreated.ts
+++ PullRequestCreated/PullRequestCreated.ts
@@ -13,19 +13,20 @@ class PullRequestCreated extends OctokitAction {
     if (pr == null) {
       return;
     }
+    let newTitle = pr.title.replace(/\s\s+/g, " ").trim();  // Mainly remove triple space between issue ID and title when copying from Jira
     const linkedIssues = pr.title?.match(JIRA_ISSUE_PATTERN) || null;
     if (linkedIssues == null) {
       const parameters = await this.newIssueParameters(pr);
       const projectKey = this.getInput('jira-project');
       const issueKey = await this.jira.createIssue(projectKey, parameters.type, pr.title, { parent: { key: parameters.parent } });
       if (issueKey != null) {
-        await this.updatePullRequestTitle(this.payload.pull_request.number, `${issueKey} ${pr.title}`);
-        await this.updatePullRequestDescription(this.payload.pull_request.number, `${this.issueLink(issueKey)}\n\n${pr.body || ''}`);
+        newTitle = `${issueKey} ${newTitle}`;
+        await this.updatePullRequestDescription(pr.number, `${this.issueLink(issueKey)}\n\n${pr.body || ''}`);
         await this.jira.moveIssue(issueKey, 'Commit');  // OPEN  -> TO DO
         await this.jira.moveIssue(issueKey, 'Start');   // TO DO -> IN PROGRESS
         const userEmail = await this.findEmail(this.payload.sender.login);
         if (userEmail != null) {
-            await this.jira.assignIssue(issueKey, userEmail);
+          await this.jira.assignIssue(issueKey, userEmail);
         }
       }
     } else {
@@ -33,8 +34,11 @@ class PullRequestCreated extends OctokitAction {
       const notMentionedIssues = linkedIssues.filter(x => !mentionedIssues.includes(x));
       console.log(`Adding the following ticket in description: ${notMentionedIssues}`);
       if (notMentionedIssues.length > 0) {
-        await this.updatePullRequestDescription(this.payload.pull_request.number, `${notMentionedIssues.map(x => this.issueLink(x)).join('\n')}\n\n${pr.body || ''}`);
+        await this.updatePullRequestDescription(pr.number, `${notMentionedIssues.map(x => this.issueLink(x)).join('\n')}\n\n${pr.body || ''}`);
       }
+    }
+    if (pr.title !== newTitle) {
+      await this.updatePullRequestTitle(pr.number, newTitle);
     }
   }
 
