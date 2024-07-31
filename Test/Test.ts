1c8ff939544f19173f3efff45747138797fedad9
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-07-31 11:19:43 +0200

    Test PR update

--- Test/Test.ts
+++ Test/Test.ts
@@ -2,12 +2,11 @@ import { OctokitAction } from '../lib/OctokitAction';
 
 class Test extends OctokitAction {
   protected async execute(): Promise<void> {
-    //const userEmail = this.getInput('login');
-    //this.log("Getting PR");
-    //const pr = await this.getPullRequest(600);
-    //this.log(`PR: ${pr.title}`);
-    //const update = { title: pr.title + '.' };
-    //await this.rest.pulls.update(this.addRepo({ pull_number: 600, ...update }));
+    this.log("Getting PR");
+    const pr = await this.getPullRequest(600);
+    this.log(`PR: ${pr.title}`);
+    const update = { title: pr.title + '.' };
+    await this.rest.pulls.update(this.addRepo({ pull_number: 600, ...update }));
 
     //this.log("Requesting getAuthenticated");
     //var me = await this.rest.users.getAuthenticated();
@@ -16,13 +15,9 @@ class Test extends OctokitAction {
 
 
 
-    this.log("findingEmail");
-
-    const userEmail = await this.findEmail(this.getInput('login'));
-
-
-
-    this.log(`Result: ${userEmail}`);
+    //this.log("findingEmail");
+    //const userEmail = await this.findEmail(this.getInput('login'));
+    //this.log(`Result: ${userEmail}`);
   }
 }
 
--- dist/Test/Test.js
+++ dist/Test/Test.js
@@ -3,19 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const OctokitAction_1 = require("../lib/OctokitAction");
 class Test extends OctokitAction_1.OctokitAction {
     async execute() {
-        //const userEmail = this.getInput('login');
-        //this.log("Getting PR");
-        //const pr = await this.getPullRequest(600);
-        //this.log(`PR: ${pr.title}`);
-        //const update = { title: pr.title + '.' };
-        //await this.rest.pulls.update(this.addRepo({ pull_number: 600, ...update }));
+        this.log("Getting PR");
+        const pr = await this.getPullRequest(600);
+        this.log(`PR: ${pr.title}`);
+        const update = { title: pr.title + '.' };
+        await this.rest.pulls.update(this.addRepo({ pull_number: 600, ...update }));
         //this.log("Requesting getAuthenticated");
         //var me = await this.rest.users.getAuthenticated();
         //this.log("Received getAuthenticated");
         //this.logSerialized(me);
-        this.log("findingEmail");
-        const userEmail = await this.findEmail(this.getInput('login'));
-        this.log(`Result: ${userEmail}`);
+        //this.log("findingEmail");
+        //const userEmail = await this.findEmail(this.getInput('login'));
+        //this.log(`Result: ${userEmail}`);
     }
 }
 const action = new Test();
