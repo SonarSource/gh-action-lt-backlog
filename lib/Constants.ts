1d976e29dca259a40a023094d70d3ad123802ff8
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-07-31 16:27:50 +0200

    Update Jira domain

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -1,2 +1,2 @@
 export const JIRA_ISSUE_PATTERN: RegExp = /[A-Z]+-\d+/g;
-export const JIRA_DOMAIN = 'https://sonarsource-sandbox-608.atlassian.net';
+export const JIRA_DOMAIN = 'https://sonarsource.atlassian.net';
