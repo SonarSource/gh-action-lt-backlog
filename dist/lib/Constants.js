7e9cba5f6685f39e9ea6254027fc5805b60b4264
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-09-09 15:50:32 +0200

    Fix Jira Issue regex pattern (#125)

--- dist/lib/Constants.js
+++ dist/lib/Constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.JIRA_DOMAIN = exports.JIRA_ISSUE_PATTERN = void 0;
-exports.JIRA_ISSUE_PATTERN = /[A-Z]+-\d+/g;
+exports.JIRA_ISSUE_PATTERN = /[A-Z][A-Z0-9]*-\d+/g;
 exports.JIRA_DOMAIN = 'https://sonarsource.atlassian.net';
 //# sourceMappingURL=Constants.js.map
