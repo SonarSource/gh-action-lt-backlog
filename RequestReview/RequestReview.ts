34203864a1b250e669b20bd9583163384b1b26e5
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-12-02 14:04:58 +0100

    Fix RequestReview for team (#129)

--- RequestReview/RequestReview.ts
+++ RequestReview/RequestReview.ts
@@ -3,9 +3,11 @@ import { PullRequestAction } from '../lib/PullRequestAction';
 class RequestReview extends PullRequestAction {
   protected async processJiraIssue(issueId: string): Promise<void> {
     await this.jira.moveIssue(issueId, 'Request Review');
-    const userEmail = await this.findEmail(this.payload.requested_reviewer.login);
-    if (userEmail != null) {
-      await this.jira.assignIssue(issueId, userEmail);
+    if (this.payload.requested_reviewer) {  // When team is requested for a review, it has this.payload.requested_team
+      const userEmail = await this.findEmail(this.payload.requested_reviewer.login);
+      if (userEmail != null) {
+        await this.jira.assignIssue(issueId, userEmail);
+      }
     }
   }
 }
