2876de3c8f5bd7eafc5ca92df38d08b17a5e0ce1
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-12-18 17:19:01 +0100

    Review update

--- dist/lib/Constants.js
+++ dist/lib/Constants.js
@@ -2,6 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.JIRA_DOMAIN = exports.JIRA_ISSUE_PATTERN = void 0;
 exports.JIRA_ISSUE_PATTERN = /[A-Z][A-Z0-9]*-\d+/g;
-// Use https://sonarsource-sandbox-608.atlassian.net for testing
 exports.JIRA_DOMAIN = 'https://sonarsource.atlassian.net';
 //# sourceMappingURL=Constants.js.map
--- lib/Constants.ts
+++ lib/Constants.ts
@@ -1,3 +1,2 @@
 export const JIRA_ISSUE_PATTERN: RegExp = /[A-Z][A-Z0-9]*-\d+/g;
-// Use https://sonarsource-sandbox-608.atlassian.net for testing
 export const JIRA_DOMAIN = 'https://sonarsource.atlassian.net';
