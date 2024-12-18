75840b907914218e30f52ed57426f4bdda36ecfe
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-12-18 16:27:37 +0100

    Sandbox

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -1,2 +1,2 @@
 export const JIRA_ISSUE_PATTERN: RegExp = /[A-Z][A-Z0-9]*-\d+/g;
-export const JIRA_DOMAIN = 'https://sonarsource.atlassian.net';
+export const JIRA_DOMAIN = 'https://sonarsource-sandbox-608.atlassian.net'; // FIXME: Debug value on a Dev branch. This should NOT make it to a PR nor master
