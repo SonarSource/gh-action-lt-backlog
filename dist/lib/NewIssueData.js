82fe4d8fe00c76eda7d07fca99c2bd1a75b29581
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-03-06 09:09:24 +0100

    Improve

--- dist/lib/NewIssueData.js
+++ dist/lib/NewIssueData.js
@@ -13,16 +13,15 @@ class NewIssueData {
         const parent = await this.findNonSubTaskParent(jira, this.findMentionedIssues(pr));
         const projectKey = this.computeProjectKey(inputJiraProject, parent);
         const accountId = await jira.findAccountId(userEmail);
-        const team = await this.findTeam(jira, accountId, projectKey); // Can be null for bots when project lead is not member of any team. Jira request will fail if the field is mandatory for the project.
-        const sprintId = await this.findSprintId(jira, team.name);
-        let additionalFields = this.parseAdditionalFields(inputAdditionFields);
+        const additionalFields = this.parseAdditionalFields(inputAdditionFields);
         const parameters = this.newIssueParameters(projectKey, parent, additionalFields.issuetype?.name ?? 'Task'); // Transfer issuetype name manually, because parameters should have priority due to Sub-task.
         if (parameters.issuetype.name !== 'Sub-task') { // These fields cannot be set on Sub-task. Their values are inherited from the parent issue.
+            const team = await this.findTeam(jira, accountId, projectKey); // Can be null for bots when project lead is not member of any team. Jira request will fail if the field is mandatory for the project.
+            const sprintId = await this.findSprintId(jira, team.name);
             parameters.customfield_10001 = team.id;
             parameters.customfield_10020 = sprintId;
         }
-        additionalFields = { ...additionalFields, ...parameters };
-        return new NewIssueData(projectKey, accountId, additionalFields);
+        return new NewIssueData(projectKey, accountId, { ...additionalFields, ...parameters });
     }
     static computeProjectKey(inputJiraProject, parent) {
         // If projectKey is not defined (like in rspec), we want only to create only Sub-tasks in other tasks (not Epics).
