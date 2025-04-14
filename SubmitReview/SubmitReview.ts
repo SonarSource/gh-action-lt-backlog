a808e73cdf312236ae9ae8a51e69d878943f5851
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-04-14 17:10:42 +0200

    GHA-16 Update SubmitReview for EngExp (#172)

--- SubmitReview/SubmitReview.ts
+++ SubmitReview/SubmitReview.ts
@@ -4,7 +4,14 @@ class SubmitReview extends PullRequestAction {
 
   protected async processJiraIssue(issueId: string): Promise<void> {
     if (this.payload.review.state === 'approved') {
-      await this.jira.moveIssue(issueId, 'Approve');
+      if (this.getInputBoolean('is-infra')) {
+        const userEmail = await this.findEmail(this.payload.sender.login);
+        if (userEmail) {
+          await this.jira.addReviewedBy(issueId, userEmail);
+        }
+      } else {
+        await this.jira.moveIssue(issueId, 'Approve');
+      }
     } else if (this.payload.review.state === 'changes_requested') {
       await this.jira.moveIssue(issueId, 'Request Changes');
     }
--- dist/SubmitReview/SubmitReview.js
+++ dist/SubmitReview/SubmitReview.js
@@ -4,7 +4,15 @@ const PullRequestAction_1 = require("../lib/PullRequestAction");
 class SubmitReview extends PullRequestAction_1.PullRequestAction {
     async processJiraIssue(issueId) {
         if (this.payload.review.state === 'approved') {
-            await this.jira.moveIssue(issueId, 'Approve');
+            if (this.getInputBoolean('is-infra')) {
+                const userEmail = await this.findEmail(this.payload.sender.login);
+                if (userEmail) {
+                    await this.jira.addReviewedBy(issueId, userEmail);
+                }
+            }
+            else {
+                await this.jira.moveIssue(issueId, 'Approve');
+            }
         }
         else if (this.payload.review.state === 'changes_requested') {
             await this.jira.moveIssue(issueId, 'Request Changes');
