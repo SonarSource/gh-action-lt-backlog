4761bb9c1958a734c19dccc3f9baa7556fe73ad8
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-04-15 16:09:24 +0200

    Refactor PullRequestCreated to prepare it

--- PullRequestCreated/PullRequestCreated.ts
+++ PullRequestCreated/PullRequestCreated.ts
@@ -13,28 +13,24 @@ class PullRequestCreated extends OctokitAction {
     if (pr == null) {
       return;
     }
-    let newTitle = pr.title.replace(/\s\s+/g, " ").trim();  // Mainly remove triple space between issue ID and title when copying from Jira
-    const linkedIssues = pr.title.match(JIRA_ISSUE_PATTERN);
+    let linkedIssues = pr.title.match(JIRA_ISSUE_PATTERN);
     if (linkedIssues == null) {
-      newTitle = await this.processNewJiraIssue(pr, newTitle);
-    } else {
-      await this.addLinkedIssuesToDescription(pr, linkedIssues);
+      linkedIssues = [await this.processNewJiraIssue(pr)];
+    } else if (pr.title !== this.cleanupWhitespace(pr.title)) { // New issues do this when persisting issue ID
+      await this.updatePullRequestTitle(pr.number, this.cleanupWhitespace(pr.title));
     }
-    if (pr.title !== newTitle) {
-      await this.updatePullRequestTitle(pr.number, newTitle);
-    }
+    await this.addLinkedIssuesToDescription(pr, linkedIssues);
   }
 
-  private async processNewJiraIssue(pr: PullRequest, newTitle: string): Promise<string> {
+  private async processNewJiraIssue(pr: PullRequest): Promise<string> {
     const data = await NewIssueData.create(this.jira, pr, this.getInput('jira-project'), this.getInput('additional-fields'), await this.findEmail(this.payload.sender.login));
     if (data) {
-      const issueId = await this.jira.createIssue(data.projectKey, pr.title, data.additionalFields);;
+      const issueId = await this.jira.createIssue(data.projectKey, pr.title, data.additionalFields);
       if (issueId == null) {
         this.setFailed('Failed to create a new issue in Jira');
-      }
-      else {
-        newTitle = `${issueId} ${newTitle}`;
-        await this.addLinkedIssuesToDescription(pr, [issueId]);
+        return null;
+      } else {
+        await this.persistIssueId(pr, issueId);
         await this.jira.moveIssue(issueId, 'Commit');   // OPEN  -> TO DO
         if (data.accountId != null) {
           await this.jira.moveIssue(issueId, 'Start');  // TO DO -> IN PROGRESS only for real accounts, no bots GHA-8
@@ -43,9 +39,20 @@ class PullRequestCreated extends OctokitAction {
         if (this.payload.pull_request.requested_reviewers.length > 0) { // When PR is created directly with a reviewer, process it here. RequestReview action can be scheduled faster and PR title might not have an issue ID yet
           await this.processRequestReview(issueId, this.payload.pull_request.requested_reviewers[0]);
         }
+        return issueId;
       }
+    } else {
+      return null;
     }
-    return newTitle;
+  }
+
+  private async persistIssueId(pr: PullRequest, issueId: string): Promise<void> {
+    pr.title = this.cleanupWhitespace(`${issueId} ${pr.title}`);
+    await this.updatePullRequestTitle(pr.number, pr.title);
+  }
+
+  private cleanupWhitespace(value: string): string {
+    return value.replace(/\s\s+/g, " ").trim();  // Mainly remove triple space between issue ID and title when copying from Jira
   }
 
   private async addLinkedIssuesToDescription(pr: PullRequest, linkedIssues: string[]): Promise<void> {
--- dist/PullRequestCreated/PullRequestCreated.js
+++ dist/PullRequestCreated/PullRequestCreated.js
@@ -13,29 +13,25 @@ class PullRequestCreated extends OctokitAction_1.OctokitAction {
         if (pr == null) {
             return;
         }
-        let newTitle = pr.title.replace(/\s\s+/g, " ").trim(); // Mainly remove triple space between issue ID and title when copying from Jira
-        const linkedIssues = pr.title.match(Constants_1.JIRA_ISSUE_PATTERN);
+        let linkedIssues = pr.title.match(Constants_1.JIRA_ISSUE_PATTERN);
         if (linkedIssues == null) {
-            newTitle = await this.processNewJiraIssue(pr, newTitle);
+            linkedIssues = [await this.processNewJiraIssue(pr)];
         }
-        else {
-            await this.addLinkedIssuesToDescription(pr, linkedIssues);
+        else if (pr.title !== this.cleanupWhitespace(pr.title)) { // New issues do this when persisting issue ID
+            await this.updatePullRequestTitle(pr.number, this.cleanupWhitespace(pr.title));
         }
-        if (pr.title !== newTitle) {
-            await this.updatePullRequestTitle(pr.number, newTitle);
-        }
+        await this.addLinkedIssuesToDescription(pr, linkedIssues);
     }
-    async processNewJiraIssue(pr, newTitle) {
+    async processNewJiraIssue(pr) {
         const data = await NewIssueData_1.NewIssueData.create(this.jira, pr, this.getInput('jira-project'), this.getInput('additional-fields'), await this.findEmail(this.payload.sender.login));
         if (data) {
             const issueId = await this.jira.createIssue(data.projectKey, pr.title, data.additionalFields);
-            ;
             if (issueId == null) {
                 this.setFailed('Failed to create a new issue in Jira');
+                return null;
             }
             else {
-                newTitle = `${issueId} ${newTitle}`;
-                await this.addLinkedIssuesToDescription(pr, [issueId]);
+                await this.persistIssueId(pr, issueId);
                 await this.jira.moveIssue(issueId, 'Commit'); // OPEN  -> TO DO
                 if (data.accountId != null) {
                     await this.jira.moveIssue(issueId, 'Start'); // TO DO -> IN PROGRESS only for real accounts, no bots GHA-8
@@ -44,9 +40,19 @@ class PullRequestCreated extends OctokitAction_1.OctokitAction {
                 if (this.payload.pull_request.requested_reviewers.length > 0) { // When PR is created directly with a reviewer, process it here. RequestReview action can be scheduled faster and PR title might not have an issue ID yet
                     await this.processRequestReview(issueId, this.payload.pull_request.requested_reviewers[0]);
                 }
+                return issueId;
             }
         }
-        return newTitle;
+        else {
+            return null;
+        }
+    }
+    async persistIssueId(pr, issueId) {
+        pr.title = this.cleanupWhitespace(`${issueId} ${pr.title}`);
+        await this.updatePullRequestTitle(pr.number, pr.title);
+    }
+    cleanupWhitespace(value) {
+        return value.replace(/\s\s+/g, " ").trim(); // Mainly remove triple space between issue ID and title when copying from Jira
     }
     async addLinkedIssuesToDescription(pr, linkedIssues) {
         console.log(`Adding the following ticket in description: ${linkedIssues}`);
