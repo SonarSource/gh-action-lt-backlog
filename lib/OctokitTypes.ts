17051a122a95d80c09ff0368b67731fa49b03d54
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-04-16 12:59:36 +0200

    Debug

--- lib/OctokitTypes.ts
+++ lib/OctokitTypes.ts
@@ -1,3 +1,7 @@
 import { components } from '@octokit/openapi-types/types.d';
 
 export type PullRequest = components['schemas']['pull-request'];
+
+PullRequest.prototype.isRenovate = function (): boolean {
+  return this.user?.login === 'renovate[bot]';
+};
