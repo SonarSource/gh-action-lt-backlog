aae5320ef17076bb7b1d318ade3439e125348046
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-04-29 08:31:59 +0200

    GHA-28 Don't search for mentioned issues for bots

--- lib/OctokitTypes.ts
+++ lib/OctokitTypes.ts
@@ -4,6 +4,7 @@ export type IssueComment = components["schemas"]["issue-comment"];
 export type PullRequest = components['schemas']['pull-request'] & {
   // Declare extensions for the underlaying type. We can't modify the prototype, unfortunately. 
   isRenovate(): boolean;
+  isDependabot(): boolean;
 };
 
 export function addPullRequestExtensions(pr: components['schemas']['pull-request']): PullRequest {  // Adds implementation of declared extensions
@@ -11,6 +12,9 @@ export function addPullRequestExtensions(pr: components['schemas']['pull-request
     ...pr,
     isRenovate: function (): boolean {
       return this.user.login === "renovate[bot]";
+    },
+    isDependabot: function (): boolean {
+      return this.user.login === "dependabot[bot]";
     }
   };
 }
