115a474766da07134e2a742c2865c3d611419471
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-06-04 12:14:45 +0200

    MarkdownTextParser.test

--- dist/lib/MarkdownTextParser.test.js
+++ dist/lib/MarkdownTextParser.test.js
@@ -1,74 +1,69 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const expect_1 = require("expect");
-test("Title", () => {
-    (0, expect_1.expect)(42).toBe(42);
+const MarkdownTextParser_1 = require("../lib/MarkdownTextParser");
+describe('MarkdownTextParser', () => {
+    test('parses plain text', () => {
+        const sut = new MarkdownTextParser_1.MarkdownTextParser('Lorem ipsum');
+        (0, expect_1.expect)(sut.readBlock()).toEqual({ type: 'text', text: 'Lorem ipsum' });
+        (0, expect_1.expect)(sut.readBlock()).toBeNull();
+    });
+    it('parses inline code', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('Before `snippet` after.');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('parses inline code chain', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('`snippet1` `snippet2``snippet3 no space before`');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet3 no space before' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('parses a link', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('Before [title](http://example.com) after.');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title', href: 'http://example.com' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('parses a link chain', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('[title1](title1) [title2 with space](link2)[title3 no space before](link3)');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'title1' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title2 with space', href: 'link2' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title3 no space before', href: 'link3' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('parses mixed content', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('Before `snippet1` [title1](url1) `snippet2` [title2](url2) after.');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'url1' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'link', text: 'title2', href: 'url2' });
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('handles unterminated code', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('`unterminated');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'code', text: 'unterminated' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('handles text with brackets not a link', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('Not a [link');
+        (0, expect_1.expect)(parser.readBlock()).toEqual({ type: 'text', text: 'Not a [link' });
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
+    it('handles empty string', () => {
+        const parser = new MarkdownTextParser_1.MarkdownTextParser('');
+        (0, expect_1.expect)(parser.readBlock()).toBeNull();
+    });
 });
-test("To fail", () => {
-    (0, expect_1.expect)(42).toBe(43);
-});
-//test('MarkdownTextParser', () => {
-//  it('parses plain text', () => {
-//    const sut = new MarkdownTextParser('Lorem ipsum');
-//    expect(sut.readBlock()).toEqual({ type: 'text', text: 'Lorem ipsum' });
-//    expect(sut.readBlock()).toBeNull();
-//  });
-//  it('parses inline code', () => {
-//    const parser = new MarkdownTextParser('Before `snippet` after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('parses inline code chain', () => {
-//    const parser = new MarkdownTextParser('`snippet1` `snippet2``snippet3 no space before`');
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet3 no space before' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('parses a link', () => {
-//    const parser = new MarkdownTextParser('Before [title](http://example.com) after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title', href: 'http://example.com' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('parses a link chain', () => {
-//    const parser = new MarkdownTextParser('[title1](title1) [title2 with space](link2)[title3 no space before](link3)');
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'title1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2 with space', href: 'link2' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title3 no space before', href: 'link3' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('parses mixed content', () => {
-//    const parser = new MarkdownTextParser('Before `snippet1` [title1](url1) `snippet2` [title2](url2) after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'url1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2', href: 'url2' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('handles unterminated code', () => {
-//    const parser = new MarkdownTextParser('`unterminated');
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'unterminated' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  it('handles text with brackets not a link', () => {
-//    const parser = new MarkdownTextParser('Not a [link');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Not a [link' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-//  //it('handles empty string', () => {
-//  //  const parser = new MarkdownTextParser('');
-//  //  expect(parser.readBlock()).toBeNull();
-//  //});
-//});
 //# sourceMappingURL=MarkdownTextParser.test.js.map
--- lib/MarkdownTextParser.test.ts
+++ lib/MarkdownTextParser.test.ts
@@ -1,86 +1,75 @@
 import { expect } from 'expect';
-import { MarkdownTextParser, TextBlock } from '../lib/MarkdownTextParser';
+import { MarkdownTextParser } from '../lib/MarkdownTextParser';
 
+describe('MarkdownTextParser', () => {
+  test('parses plain text', () => {
+    const sut = new MarkdownTextParser('Lorem ipsum');
+    expect(sut.readBlock()).toEqual({ type: 'text', text: 'Lorem ipsum' });
+    expect(sut.readBlock()).toBeNull();
+  });
 
+  it('parses inline code', () => {
+    const parser = new MarkdownTextParser('Before `snippet` after.');
+    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+    expect(parser.readBlock()).toBeNull();
+  });
 
-test("Title", () => {
-  expect(42).toBe(42);
+  it('parses inline code chain', () => {
+    const parser = new MarkdownTextParser('`snippet1` `snippet2``snippet3 no space before`');
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet3 no space before' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('parses a link', () => {
+    const parser = new MarkdownTextParser('Before [title](http://example.com) after.');
+    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title', href: 'http://example.com' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('parses a link chain', () => {
+    const parser = new MarkdownTextParser('[title1](title1) [title2 with space](link2)[title3 no space before](link3)');
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'title1' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2 with space', href: 'link2' });
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title3 no space before', href: 'link3' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('parses mixed content', () => {
+    const parser = new MarkdownTextParser('Before `snippet1` [title1](url1) `snippet2` [title2](url2) after.');
+    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'url1' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
+    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2', href: 'url2' });
+    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('handles unterminated code', () => {
+    const parser = new MarkdownTextParser('`unterminated');
+    expect(parser.readBlock()).toEqual({ type: 'code', text: 'unterminated' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('handles text with brackets not a link', () => {
+    const parser = new MarkdownTextParser('Not a [link');
+    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Not a [link' });
+    expect(parser.readBlock()).toBeNull();
+  });
+
+  it('handles empty string', () => {
+    const parser = new MarkdownTextParser('');
+    expect(parser.readBlock()).toBeNull();
+  });
 });
-
-test("To fail", () => {
-  expect(42).toBe(43);
-});
-
-
-//test('MarkdownTextParser', () => {
-//  it('parses plain text', () => {
-//    const sut = new MarkdownTextParser('Lorem ipsum');
-//    expect(sut.readBlock()).toEqual({ type: 'text', text: 'Lorem ipsum' });
-//    expect(sut.readBlock()).toBeNull();
-//  });
-
-//  it('parses inline code', () => {
-//    const parser = new MarkdownTextParser('Before `snippet` after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('parses inline code chain', () => {
-//    const parser = new MarkdownTextParser('`snippet1` `snippet2``snippet3 no space before`');
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet3 no space before' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('parses a link', () => {
-//    const parser = new MarkdownTextParser('Before [title](http://example.com) after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title', href: 'http://example.com' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('parses a link chain', () => {
-//    const parser = new MarkdownTextParser('[title1](title1) [title2 with space](link2)[title3 no space before](link3)');
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'title1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2 with space', href: 'link2' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title3 no space before', href: 'link3' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('parses mixed content', () => {
-//    const parser = new MarkdownTextParser('Before `snippet1` [title1](url1) `snippet2` [title2](url2) after.');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Before ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title1', href: 'url1' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'snippet2' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' ' });
-//    expect(parser.readBlock()).toEqual({ type: 'link', text: 'title2', href: 'url2' });
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: ' after.' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('handles unterminated code', () => {
-//    const parser = new MarkdownTextParser('`unterminated');
-//    expect(parser.readBlock()).toEqual({ type: 'code', text: 'unterminated' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  it('handles text with brackets not a link', () => {
-//    const parser = new MarkdownTextParser('Not a [link');
-//    expect(parser.readBlock()).toEqual({ type: 'text', text: 'Not a [link' });
-//    expect(parser.readBlock()).toBeNull();
-//  });
-
-//  //it('handles empty string', () => {
-//  //  const parser = new MarkdownTextParser('');
-//  //  expect(parser.readBlock()).toBeNull();
-//  //});
-//});
