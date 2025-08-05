96809fa38d9cd107be81e038fa02088382f611df
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-08-05 14:50:36 +0200

    GHA-80 Add UTs for PullRequestCreated

--- dist/PullRequestCreated/PullRequestCreated.test.js
+++ dist/PullRequestCreated/PullRequestCreated.test.js
@@ -42,6 +42,11 @@ describe('PullRequestCreated', () => {
                 body: 'PR Description',
                 requested_reviewers: []
             },
+            repository: {
+                html_url: "https://github.com/test-owner/test-repo",
+                name: 'test-repo',
+                owner: null
+            },
             sender: {
                 login: 'test-user',
                 type: "User"
@@ -97,7 +102,7 @@ describe('PullRequestCreated', () => {
         expect(action.log).toHaveBeenCalledWith('Done');
         expect(action.log).toHaveBeenCalledTimes(1);
     });
-    it('Standalone PR simple', async () => {
+    it('Standalone PR no description', async () => {
         await runAction('KEY', 'Standalone PR');
         expect(logTester.logsParams).toStrictEqual([
             "Loading PR #42",
@@ -117,20 +122,110 @@ describe('PullRequestCreated', () => {
             "Done"
         ]);
     });
-    it.skip('Standalone PR Renovate', async () => {
-        // ToDo: GHA-80
+    it('Standalone PR with description', async () => {
+        github.context.payload.pull_request.requested_teams = { type: 'Team', name: 'test-team' }; // Action does nothing additional when team is (auto)requested for review
+        await runAction('KEY', 'Standalone PR', 'Original description');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "No mentioned issues found",
+            "Looking for a non-Sub-task ticket",
+            "No parent issue found",
+            "Invoked jira.createIssue('KEY', 'Standalone PR', {\"issuetype\":{\"name\":\"Task\"},\"customfield_10001\":\"dot-neeet-team\",\"customfield_10020\":42})",
+            "Updating PR #42 title to: KEY-4242 Standalone PR",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"title\":\"KEY-4242 Standalone PR\"})",
+            "Invoked jira.addIssueRemoteLink('KEY-4242'', 'https://github.com/test-owner/test-repo/pull/42', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Commit', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Start', null)",
+            "Invoked jira.assignIssueToAccount('KEY-4242', '1234-account')",
+            "Adding the following ticket in description: KEY-4242",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[KEY-4242](https://sonarsource.atlassian.net/browse/KEY-4242)\\n\\nOriginal description\"})",
+            "Done"
+        ]);
     });
-    it.skip('Standalone PR with reviewer', async () => {
-        // ToDo: GHA-80
+    it('Standalone PR Renovate', async () => {
+        await runAction('KEY', 'Standalone PR', null, 'renovate[bot]');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "Invoked jira.createIssue('KEY', 'Standalone PR', {\"issuetype\":{\"name\":\"Task\"},\"customfield_10001\":\"dot-neeet-team\",\"customfield_10020\":42})",
+            "Invoked rest.issues.createComment({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"issue_number\":42,\"body\":\"Renovate Jira issue ID: [KEY-4242](https://sonarsource.atlassian.net/browse/KEY-4242)\"})",
+            "Invoked jira.addIssueRemoteLink('KEY-4242'', 'https://github.com/test-owner/test-repo/pull/42', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Commit', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Start', null)",
+            "Invoked jira.assignIssueToAccount('KEY-4242', '1234-account')",
+            "Adding the following ticket in description: KEY-4242",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[KEY-4242](https://sonarsource.atlassian.net/browse/KEY-4242)\\n\\n\"})",
+            "Done"
+        ]);
     });
-    it.skip('Standalone PR isEngXpSquad', async () => {
-        // ToDo: GHA-80
+    it('Standalone PR with reviewer', async () => {
+        github.context.payload.pull_request.requested_reviewers = [{ type: "User", login: "test-user" }];
+        await runAction('KEY', 'Standalone PR');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "No mentioned issues found",
+            "Looking for a non-Sub-task ticket",
+            "No parent issue found",
+            "Invoked jira.createIssue('KEY', 'Standalone PR', {\"issuetype\":{\"name\":\"Task\"},\"customfield_10001\":\"dot-neeet-team\",\"customfield_10020\":42})",
+            "Updating PR #42 title to: KEY-4242 Standalone PR",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"title\":\"KEY-4242 Standalone PR\"})",
+            "Invoked jira.addIssueRemoteLink('KEY-4242'', 'https://github.com/test-owner/test-repo/pull/42', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Commit', null)",
+            "Invoked jira.moveIssue('KEY-4242', 'Start', null)",
+            "Invoked jira.assignIssueToAccount('KEY-4242', '1234-account')",
+            "Invoked jira.moveIssue('KEY-4242', 'Request Review', null)",
+            "Invoked jira.assignIssueToEmail('KEY-4242', 'user@sonarsource.com')",
+            "Adding the following ticket in description: KEY-4242",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[KEY-4242](https://sonarsource.atlassian.net/browse/KEY-4242)\\n\\n\"})",
+            "Done"
+        ]);
     });
-    it.skip('Normal PR cleans up title ', async () => {
-        // ToDo: GHA-80
+    it('Standalone PR isEngXpSquad', async () => {
+        process.env['INPUT_IS-ENG-XP-SQUAD'] = 'true';
+        await runAction('', 'Standalone PR');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "Invoked jira.createIssue('PREQ', 'Standalone PR', {\"issuetype\":{\"name\":\"Task\"},\"reporter\":{\"id\":\"1234-account\"},\"customfield_10001\":\"eb40f25e-3596-4541-b661-cf83e7bc4fa6\",\"customfield_10020\":42,\"labels\":[\"dvi-created-by-automation\"]})",
+            "Updating PR #42 title to: PREQ-4242 Standalone PR",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"title\":\"PREQ-4242 Standalone PR\"})",
+            "Invoked jira.addIssueRemoteLink('PREQ-4242'', 'https://github.com/test-owner/test-repo/pull/42', null)",
+            "Invoked jira.moveIssue('PREQ-4242', 'Commit', null)",
+            "Invoked jira.moveIssue('PREQ-4242', 'Start', null)",
+            "Invoked jira.assignIssueToAccount('PREQ-4242', '1234-account')",
+            "Adding the following ticket in description: PREQ-4242",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[PREQ-4242](https://sonarsource.atlassian.net/browse/PREQ-4242)\\n\\n\"})",
+            "Invoked jira.createComponent('PREQ', 'test-repo', 'https://github.com/test-owner/test-repo')",
+            "Invoked jira.addIssueComponent('PREQ-4242', 'test-repo')",
+            "Done"
+        ]);
     });
-    it.skip('Normal PR isEngXpSquad', async () => {
-        // ToDo: GHA-80
+    it('Normal PR cleans up title ', async () => {
+        await runAction('KEY', '    GHA-1000    Useless whitespace ');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "Updating PR #42 title to: GHA-1000 Useless whitespace",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"title\":\"GHA-1000 Useless whitespace\"})",
+            "Adding the following ticket in description: GHA-1000",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[GHA-1000](https://sonarsource.atlassian.net/browse/GHA-1000)\\n\\n\"})",
+            "Done"
+        ]);
+    });
+    it('Normal PR isEngXpSquad', async () => {
+        process.env['INPUT_IS-ENG-XP-SQUAD'] = 'true';
+        await runAction('', 'BUILD-4444 Fix normal issue');
+        expect(logTester.logsParams).toStrictEqual([
+            "Loading PR #42",
+            "Adding the following ticket in description: BUILD-4444",
+            "Updating PR #42 description",
+            "Invoked rest.pulls.update({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"pull_number\":42,\"body\":\"[BUILD-4444](https://sonarsource.atlassian.net/browse/BUILD-4444)\\n\\n\"})",
+            "Invoked jira.createComponent('BUILD', 'test-repo', 'https://github.com/test-owner/test-repo')",
+            "Invoked jira.addIssueComponent('BUILD-4444', 'test-repo')",
+            "Done"
+        ]);
     });
 });
 //# sourceMappingURL=PullRequestCreated.test.js.map
--- tests/OctokitRestStub.ts
+++ tests/OctokitRestStub.ts
@@ -1,4 +1,8 @@
 type RestStub = {
+  issues: {
+    createComment: (params: any) => void,
+    listComments: (params: any) => any
+  }
   pulls: {
     get: (params: any) => any,
     update: (params: any) => void
@@ -7,6 +11,16 @@ type RestStub = {
 
 export function createOctokitRestStub(title: string, body?: string, login: string = 'test-user'): RestStub {
   return {
+    issues: {
+      createComment(params: any): void {
+        console.log(`Invoked rest.issues.createComment(${JSON.stringify(params)})`);
+      },
+      listComments(params: any): any {
+        return {
+          data: []
+        }
+      }
+    },
     pulls: {
       get(params: any): any {
         return {
@@ -15,6 +29,11 @@ export function createOctokitRestStub(title: string, body?: string, login: strin
             title,
             body,
             html_url: 'https://github.com/test-owner/test-repo/pull/42',
+            base: {
+              repo: {
+                name: 'test-repo'
+              }
+            },
             user: {
               login,
             }
