3530ec0f261bb3f7a4d71be1808b4e2d909408a0
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-11-11 15:17:49 +0100

    Add license headers

--- Data/TeamConfiguration.test.ts
+++ Data/TeamConfiguration.test.ts
@@ -1,4 +1,24 @@
-﻿import { JIRA_DOMAIN, JIRA_ORGANIZATION_ID, JIRA_SITE_ID } from "../lib/Constants";
+﻿/*
+ * Backlog Automation
+ * Copyright (C) 2022-2025 SonarSource Sàrl
+ * mailto: info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import { JIRA_DOMAIN, JIRA_ORGANIZATION_ID, JIRA_SITE_ID } from "../lib/Constants";
 import { JiraClient } from "../lib/JiraClient";
 import { LogTester } from "../tests/LogTester";
 import { EngineeringExperienceSquad, TeamConfigurationData } from "./TeamConfiguration";
