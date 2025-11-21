c948b69da185ff0f7783502aa4227a8ea29420e3
Pavel Mikula <pavel.mikula@sonarsource.com>
2025-11-21 10:10:12 +0100

    Handle In Progress column

--- dist/TrelloSync/TrelloSync.js
+++ dist/TrelloSync/TrelloSync.js
@@ -12,34 +12,36 @@ class TrelloSync extends OctokitAction_1.OctokitAction {
     async execute() {
         const pattern = /^[A-Z][A-Z0-9]*-\d+/g;
         const log = [];
-        for (const card of await this.listCards('VXbSw1ia', 'Planned')) {
-            if (card.start && card.due) {
-                const ids = card.name.match(pattern);
-                if (ids && ids.length === 1) {
-                    const issue = await this.jira.loadIssue(ids[0]);
-                    if (issue) {
-                        if (!issue.fields.customfield_10001) {
-                            log.push(`Epic is missing Team: ${card.name}`);
-                        }
-                        const start = new Date(card.start).toISOString().split('T')[0];
-                        const due = new Date(card.due).toISOString().split('T')[0];
-                        if (issue.fields.customfield_10015 === start && issue.fields.duedate === due) {
-                            this.log(`SKIP already up to date: ${card.name}`);
+        for (const column of ['In Progress', 'Planned']) {
+            for (const card of await this.listCards('VXbSw1ia', column)) {
+                if (card.start && card.due) {
+                    const ids = card.name.match(pattern);
+                    if (ids?.length === 1) {
+                        const issue = await this.jira.loadIssue(ids[0]);
+                        if (issue) {
+                            if (!issue.fields.customfield_10001) {
+                                log.push(`Epic is missing Team: ${card.name}`);
+                            }
+                            const start = new Date(card.start).toISOString().split('T')[0];
+                            const due = new Date(card.due).toISOString().split('T')[0];
+                            if (issue.fields.customfield_10015 === start && issue.fields.duedate === due) {
+                                this.log(`SKIP already up to date: ${card.name}`);
+                            }
+                            else {
+                                await this.updateJiraIssue(ids[0], start, due);
+                            }
                         }
                         else {
-                            await this.updateJiraIssue(ids[0], start, due);
+                            log.push(`SKIP nonexistent id: ${card.name}`);
                         }
                     }
                     else {
-                        log.push(`SKIP nonexistent id: ${card.name}`);
+                        log.push(`SKIP missing id: ${card.name}`);
                     }
                 }
                 else {
-                    log.push(`SKIP missing id: ${card.name}`);
+                    log.push(`SKIP date: ${card.name}`);
                 }
-            }
-            else {
-                log.push(`SKIP date: ${card.name}`);
             }
         }
         this.log('--- ALL ---');
