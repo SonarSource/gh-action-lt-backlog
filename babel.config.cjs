91255213debe750065df40b213ddca8cbebb08f9
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-04-20 09:56:49 +0200

    Reconfigure project to vitest

--- babel.config.cjs
+++
@@ -1,7 +0,0 @@
-// Needed for Jest test framework to work with TypeScript
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
-};
