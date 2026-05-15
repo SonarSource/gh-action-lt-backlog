361d88b9a96d1f18fb288c3882558756a15ef8e6
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-05-15 14:02:26 +0200

    Extract LockBranchAction

--- ToggleLockBranch/ToggleLockBranch.test.ts
+++ ToggleLockBranch/ToggleLockBranch.test.ts
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Backlog Automation
  * Copyright (C) SonarSource Sàrl
  * mailto: info AT sonarsource DOT com
@@ -23,180 +23,27 @@ import { ToggleLockBranch } from './ToggleLockBranch.js';
 import { LogTester } from '../tests/LogTester.js';
 import { createOctokitRestStub } from '../tests/OctokitRestStub.js';
 import { OctokitActionStub } from '../tests/OctokitActionStub.js';
+import { LockBranchActionStub } from '../tests/LockBranchActionStub.js';
 
-function sendGraphQLStub(query: string): any {
-  const graphQL: { name: string, query: string, response: any }[] = [
-    {
-      name: 'list branch protection rules',
-      query: `
-        query {
-          repository(owner: "test-owner", name: "test-repo") {
-            branchProtectionRules(first: 100) {
-              nodes {
-                id
-                lockBranch
-                pattern
-              }
-            }
-          }
-        }`,
-      response: {
-        repository: {
-          branchProtectionRules: {
-            nodes: [
-              { id: 'id-of-feature-11', lockBranch: false, pattern: 'feature/*' },
-              { id: 'id-of-locked-222', lockBranch: true, pattern: 'locked' },
-              { id: 'id-of-unlocked-3', lockBranch: false, pattern: 'unlocked' },
-            ]
-          },
-        },
-      }
-    },
-    {
-      name: 'updateBranchProtectionRule to lock id-of-unlocked-3',
-      query: `
-        mutation {
-          updateBranchProtectionRule(input:{
-            branchProtectionRuleId: "id-of-unlocked-3",
-            lockBranch: true
-          })
-          {
-            branchProtectionRule {
-              id
-              lockBranch
-              pattern
-            }
-          }
-        }`,
-      response: {
-        updateBranchProtectionRule: {
-          branchProtectionRule: { id: 'id-of-unlocked-3', lockBranch: true, pattern: 'unlocked' }
-        }
-      }
-    },
-    {
-      name: 'updateBranchProtectionRule to unlock id-of-locked-222',
-      query: `
-        mutation {
-          updateBranchProtectionRule(input:{
-            branchProtectionRuleId: "id-of-locked-222",
-            lockBranch: false
-          })
-          {
-            branchProtectionRule {
-              id
-              lockBranch
-              pattern
-            }
-          }
-        }`,
-      response: {
-        updateBranchProtectionRule: {
-          branchProtectionRule: { id: 'id-of-locked-222', lockBranch: false, pattern: 'locked' }
-        }
-      }
-    },
-    {
-      name: 'list open pullrequests, page 1',
-      query: `
-        query {
-          repository(owner: "test-owner", name: "test-repo") {
-            pullRequests(first: 100, after: "", states: OPEN, baseRefName:"locked"){
-              nodes {
-                id
-                number
-                autoMergeRequest { commitHeadline } # We don't need commitHeadline, but GraphQL syntax requires to query something there
-              }
-              pageInfo {
-                endCursor
-                hasNextPage
-              }
-            }
-          }
-        }`,
-      response: {
-        repository: {
-          pullRequests: {
-            nodes: [
-              { id: 'pr-11', number: 11, autoMergeRequest: null },
-              { id: 'pr-12', number: 12, autoMergeRequest: { commitHeadline: 'Auto-merge PR #12' } }
-            ],
-            pageInfo: { endCursor: "end-of-page-1", hasNextPage: true }
-          },
-        },
-      }
-    },
-    {
-      name: 'list open pullrequests, page 2',
-      query: `
-        query {
-          repository(owner: "test-owner", name: "test-repo") {
-            pullRequests(first: 100, after: "end-of-page-1", states: OPEN, baseRefName:"locked"){
-              nodes {
-                id
-                number
-                autoMergeRequest { commitHeadline } # We don't need commitHeadline, but GraphQL syntax requires to query something there
-              }
-              pageInfo {
-                endCursor
-                hasNextPage
-              }
-            }
-          }
-        }`,
-      response: {
-        repository: {
-          pullRequests: {
-            nodes: [
-              { id: 'pr-21', number: 21, autoMergeRequest: { commitHeadline: 'Auto-merge PR #21' } },
-              { id: 'pr-22', number: 22, autoMergeRequest: null }
-            ],
-            pageInfo: { endCursor: "end-of-page-2", hasNextPage: false }
-          }
-        }
-      }
-    },
-    {
-      name: 'disablePullRequestAutoMerge for pr-12',
-      query: `
-        mutation {
-          disablePullRequestAutoMerge(input: { pullRequestId: "pr-12" } )
-          {
-            pullRequest { id }
-          }
-        }`,
-      response: {}
-    },
-    {
-      name: 'disablePullRequestAutoMerge for pr-21',
-      query: `
-        mutation {
-          disablePullRequestAutoMerge(input: { pullRequestId: "pr-21" } )
-          {
-            pullRequest { id }
-          }
-        }`,
-      response: {}
-    },
-  ];
-  const trimmedQuery = query.replaceAll(/^\s+/gm, '');
-  const request = graphQL.find(x => x.query.replaceAll(/^\s+/gm, '') === trimmedQuery);
-  if (request) {
-    console.log(`Invoked sendGraphQL ${request.name}`);
-    return request.response;
-  } else {
-    throw new Error(`Unexpected query:\n${query}`)
-  }
-}
-
-async function runAction(): Promise<void> {
-  const action = new ToggleLockBranch() as ToggleLockBranch & OctokitActionStub;
-  action.sendGraphQL = sendGraphQLStub;
-  action.rest = createOctokitRestStub("Irrelevant");
-  action.sendSlackPost = async function (url: string, jsonRequest: any): Promise<any> {
-    console.log(`Invoked sendSlackPost('${url}', ${JSON.stringify(jsonRequest)}`)
+async function runAction(currentLockBranch: boolean): Promise<void> {
+  const pattern = process.env['INPUT_BRANCH-PATTERN']!;
+  const action = new ToggleLockBranch() as unknown as LockBranchActionStub & OctokitActionStub & { run(): Promise<void> };
+  action.findRule = async (pattern) => {
+    console.log(`Invoked findRule(${pattern})`);
+    return { id: 'rule-id', lockBranch: currentLockBranch, pattern };
+  };
+  action.updateRule = async (id, lockBranch) => {
+    console.log(`Invoked updateRule(${id}, ${lockBranch})`);
+    return { id, lockBranch, pattern };
+  };
+  action.cancelAutoMerge = async (pattern) => {
+    console.log(`Invoked cancelAutoMerge(${pattern})`);
+  };
+  action.rest = createOctokitRestStub('Irrelevant');
+  action.sendSlackPost = async (url, req) => {
+    console.log(`Invoked sendSlackPost`);
     return {};
-  }
+  };
   await action.run();
 }
 
@@ -207,69 +54,34 @@ describe('ToggleLockBranch', () => {
     logTester = new LogTester();
     process.env['GITHUB_REPOSITORY'] = 'test-owner/test-repo';
     process.env['INPUT_GITHUB-TOKEN'] = 'fake';
+    process.env['INPUT_BRANCH-PATTERN'] = 'master';
+    process.env['INPUT_SLACK-CHANNEL'] = '';
+    process.env['INPUT_ADDITIONAL-MESSAGE'] = '';
   });
 
   afterEach(() => {
     logTester?.afterEach(); // When beforeAll fails, beforeEach is not called, but afterEach is.
   });
 
-  it('Missing branch protection', async () => {
-    process.env['INPUT_BRANCH-PATTERN'] = 'nonexistent';
-    await runAction();
+  it('Toggle unlocked to locked', async () => {
+    await runAction(false);
     expect(logTester.logsParams).toStrictEqual([
-      "Invoked sendGraphQL list branch protection rules",
-      "Branch protection rule with pattern 'nonexistent' does not exist.",
-      "Done"
-    ]);
-  });
-
-  it('Lock unlocked without slack', async () => {
-    process.env['INPUT_BRANCH-PATTERN'] = 'unlocked';
-    await runAction();
-    expect(logTester.logsParams).toStrictEqual([
-      "Invoked sendGraphQL list branch protection rules",
-      "Invoked sendGraphQL updateBranchProtectionRule to lock id-of-unlocked-3",
-      "Done: test-repo: The branch `unlocked` was locked :ice_cube:",
+      "Invoked findRule(master)",
+      "Invoked updateRule(rule-id, true)",
+      "Done: test-repo: The branch `master` was locked :ice_cube:",
       "Skip sending slack message, channel was not set.",
       "Done"
     ]);
   });
 
-  it('Lock unlocked with slack', async () => {
-    process.env['INPUT_BRANCH-PATTERN'] = 'unlocked';
-    process.env['INPUT_SLACK-CHANNEL'] = 'channel-name';
-    process.env['INPUT_ADDITIONAL-MESSAGE'] = 'This is because ABC';
-    await runAction();
+  it('Toggle locked to unlocked', async () => {
+    await runAction(true);
     expect(logTester.logsParams).toStrictEqual([
-      "Invoked sendGraphQL list branch protection rules",
-      "Invoked sendGraphQL updateBranchProtectionRule to lock id-of-unlocked-3", "Done: test-repo: The branch `unlocked` was locked :ice_cube:\n\nThis is because ABC",
-      "Sending Slack message",
-      "Invoked sendSlackPost('https://slack.com/api/chat.postMessage', {\"channel\":\"channel-name\",\"text\":\"test-repo: The branch `unlocked` was locked :ice_cube:\\n\\nThis is because ABC\"}",
-      "Done"
-    ]);
-  });
-
-  it('Unlock locked and cancel auto-merge', async () => {
-    process.env['INPUT_BRANCH-PATTERN'] = 'locked';
-    process.env['INPUT_SLACK-CHANNEL'] = 'channel-name';
-    process.env['INPUT_ADDITIONAL-MESSAGE'] = '';
-    await runAction();
-    expect(logTester.logsParams).toStrictEqual([
-      "Invoked sendGraphQL list branch protection rules",
-      "Canceling auto-merge for branch 'locked'",
-      "Invoked sendGraphQL list open pullrequests, page 1",
-      "Invoked sendGraphQL list open pullrequests, page 2",
-      "Found 4 PRs targeting locked, and 2 with auto-merge.",
-      "Canceling auto-merge for PR #12",
-      "Invoked sendGraphQL disablePullRequestAutoMerge for pr-12",
-      "Invoked rest.issues.createComment({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"issue_number\":12,\"body\":\"The target branch was unlocked and auto-merge was canceled to prevent unexpected actions.\"})",
-      "Canceling auto-merge for PR #21",
-      "Invoked sendGraphQL disablePullRequestAutoMerge for pr-21",
-      "Invoked rest.issues.createComment({\"owner\":\"test-owner\",\"repo\":\"test-repo\",\"issue_number\":21,\"body\":\"The target branch was unlocked and auto-merge was canceled to prevent unexpected actions.\"})",
-      "Invoked sendGraphQL updateBranchProtectionRule to unlock id-of-locked-222",
-      "Done: test-repo: The branch `locked` was unlocked and is now open for changes :sunny:",
-      "Sending Slack message",
-      "Invoked sendSlackPost('https://slack.com/api/chat.postMessage', {\"channel\":\"channel-name\",\"text\":\"test-repo: The branch `locked` was unlocked and is now open for changes :sunny:\"}",
+      "Invoked findRule(master)",
+      "Invoked cancelAutoMerge(master)",
+      "Invoked updateRule(rule-id, false)",
+      "Done: test-repo: The branch `master` was unlocked and is now open for changes :sunny:",
+      "Skip sending slack message, channel was not set.",
       "Done"
     ]);
   });
---
+++ lib/LockBranchAction.ts
@@ -0,0 +1,166 @@
+/*
+ * Backlog Automation
+ * Copyright (C) SonarSource Sàrl
+ * mailto: info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import type { GraphQlQueryResponseData } from '@octokit/graphql';
+import { OctokitAction } from './OctokitAction.js';
+
+export type ProtectionRule = {
+  id: string;
+  lockBranch: boolean;
+  pattern: string;
+};
+
+type PullRequest = {
+  id: string;
+  number: number;
+  autoMergeRequest: { commitHeadLine: string };
+};
+
+export abstract class LockBranchAction extends OctokitAction {
+  protected abstract resolveLockBranch(rule: ProtectionRule): boolean;
+
+  protected async execute(): Promise<void> {
+    const pattern = this.inputString('branch-pattern');
+    const additionalMessage = this.inputString('additional-message');
+    const rule = await this.findRule(pattern);
+    if (rule) {
+      const lockBranch = this.resolveLockBranch(rule);
+      if (rule.lockBranch === lockBranch) {
+        this.log(`The branch \`${pattern}\` is already ${lockBranch ? 'locked' : 'unlocked'}.`);
+      } else {
+        if (!lockBranch) {  // Was locked => unlocking
+          await this.cancelAutoMerge(pattern);
+        }
+        const updated = await this.updateRule(rule.id, lockBranch);
+        if (updated.lockBranch === lockBranch) {
+          const action = lockBranch ? 'locked :ice_cube:' : 'unlocked and is now open for changes :sunny:';
+          const suffix = additionalMessage ? `\n\n${additionalMessage}` : '';
+          const message = `${this.repo.repo}: The branch \`${pattern}\` was ${action}${suffix}`;
+          this.log(`Done: ${message}`);
+          this.sendSlackMessage(message);
+        } else {
+          this.log(`Failed: '${pattern}' was not updated successfully.`); // And we have no idea why
+        }
+      }
+    }
+  }
+
+  private async findRule(pattern: string): Promise<ProtectionRule | null> {
+    const rules = (await this.loadRules()).filter(x => x.pattern === pattern);
+    if (rules.length === 0) {
+      this.log(`Branch protection rule with pattern '${pattern}' does not exist.`);
+      return null;
+    } else {
+      return rules[0];
+    }
+  }
+
+  private async loadRules(): Promise<ProtectionRule[]> {
+    const {
+      repository: {
+        branchProtectionRules: { nodes },
+      },
+    }: GraphQlQueryResponseData = await this.sendGraphQL(`
+      query {
+        repository(owner: "${this.repo.owner}", name: "${this.repo.repo}") {
+          branchProtectionRules(first: 100) {
+            nodes {
+              id
+              lockBranch
+              pattern
+            }
+          }
+        }
+      }`);
+    return nodes;
+  }
+
+  private async updateRule(id: string, lockBranch: boolean): Promise<ProtectionRule> {
+    const {
+      updateBranchProtectionRule: { branchProtectionRule },
+    }: GraphQlQueryResponseData = await this.sendGraphQL(`
+      mutation {
+        updateBranchProtectionRule(input:{
+          branchProtectionRuleId: "${id}",
+          lockBranch: ${lockBranch}
+        })
+        {
+          branchProtectionRule {
+            id
+            lockBranch
+            pattern
+          }
+        }
+      }`);
+    return branchProtectionRule;
+  }
+
+  private async cancelAutoMerge(branch: string): Promise<void> {
+    this.log(`Canceling auto-merge for branch '${branch}'`);
+    const targetPRs = await this.loadPullRequests(branch);
+    const autoClosePRs = targetPRs.filter(x => x.autoMergeRequest);
+    this.log(`Found ${targetPRs.length} PRs targeting ${branch}, and ${autoClosePRs.length} with auto-merge.`);
+    for (const pr of autoClosePRs) {
+      await this.cancelPullRequestAutoMerge(pr);
+      await this.addComment(pr.number, `The target branch was unlocked and auto-merge was canceled to prevent unexpected actions.`);
+    }
+  }
+
+  private async loadPullRequests(targetBranch: string): Promise<PullRequest[]> {
+    const allNodes: PullRequest[] = [];
+    let after: string = "";
+    while (after !== null) {
+      const {
+        repository: {
+          pullRequests: { nodes, pageInfo: { endCursor, hasNextPage } },
+        },
+      }: GraphQlQueryResponseData = await this.sendGraphQL(`
+        query {
+          repository(owner: "${this.repo.owner}", name: "${this.repo.repo}") {
+            pullRequests(first: 100, after: "${after}", states: OPEN, baseRefName:"${targetBranch}"){
+              nodes {
+                id
+                number
+                autoMergeRequest { commitHeadline } # We don't need commitHeadline, but GraphQL syntax requires to query something there
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
+            }
+          }
+        }`);
+      allNodes.push(...nodes);
+      after = hasNextPage ? endCursor : null;
+    }
+    return allNodes;
+  }
+
+  private async cancelPullRequestAutoMerge(pr: PullRequest): Promise<void> {
+    this.log(`Canceling auto-merge for PR #${pr.number}`);
+    await this.sendGraphQL(`
+      mutation {
+        disablePullRequestAutoMerge(input: { pullRequestId: "${pr.id}" } )
+        {
+          pullRequest { id }
+        }
+      }`);
+  }
+}
