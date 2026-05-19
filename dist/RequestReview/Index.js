3db1b486e8d8280236459475d3f47a4c30981df0
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-05-19 14:48:23 +0200

    2

--- dist/RequestReview/Index.js
+++ dist/RequestReview/Index.js
@@ -24,7 +24,7 @@ import { JIRA_DOMAIN, JIRA_ORGANIZATION_ID, JIRA_SITE_ID } from "../lib/Constant
 import { JiraClient } from "../lib/JiraClient.js";
 import { NewIssueData } from "../lib/NewIssueData.js";
 import { TeamReviewData } from "../lib/TeamReviewData.js";
-console.log('No Parent & Eng Xp');
+console.log('No Parent & Eng Xp 2');
 const jira = new JiraClient(JIRA_DOMAIN, JIRA_SITE_ID, JIRA_ORGANIZATION_ID, process.env['JIRA_USER'] || '', process.env['JIRA_TOKEN'] || '');
 const teamReview = TeamReviewData.createFromAccount({ name: 'platform-cloud-eng-squad' }, '5dc3f7c6e3cc320c5e8a91f1');
 if (teamReview) {
@@ -33,7 +33,7 @@ if (teamReview) {
         //data.additionalFields.customfield_10001 = undefined;
         //data.additionalFields.customfield_10001 = '3ca60b21-53c7-48e2-a2e2-6e85b39551d0';  // .NET
         data.additionalFields.customfield_10001 = 'eb40f25e-3596-4541-b661-cf83e7bc4fa6'; // Eng xp
-        data.additionalFields.parent = undefined;
+        //data.additionalFields.parent = undefined;
         //data.additionalFields.parent = { key: 'SC-46721' };
         const issue = await jira.createIssue(data.projectKey, `PR review for Test`, data.additionalFields);
         console.log(`Done: ${issue}`);
