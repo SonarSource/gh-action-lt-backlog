7c74a21a693046e86cd5e569ec3125a30d907195
Pavel Mikula <pavel.mikula@sonarsource.com>
2026-06-02 13:55:29 +0200

    Shifts

--- Debug/Debug.ts
+++ Debug/Debug.ts
@@ -52,8 +52,11 @@ export class Debug extends OctokitAction {
     // const data = await this.sendRootlyGet(`schedules/a8f6f785-aea9-4647-8200-f249dfd5fa70`);
     // this.logSerialized(data);
 
-    const data = await this.sendRootlyGet(`users/29784/email_addresses`);
+    const data = await this.sendRootlyGet(`schedules/a8f6f785-aea9-4647-8200-f249dfd5fa70/shifts`);
     this.logSerialized(data);
+
+    // const data = await this.sendRootlyGet(`users/29784/email_addresses`);
+    // this.logSerialized(data);
 
 
     return null;
